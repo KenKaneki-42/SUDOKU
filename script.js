0477ef7350111278097d298d38911e24d5e08d0d
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2022-11-22 03:26:24 +0100

    Update script.js
    initialisation, déclaration des variables, créations des tableaux pour la grille de sudoku, calculs

--- script.js
+++ script.js
@@ -1,6 +1,78 @@
 function generateSudoku()
 {
-    // @TODO
+  // *** Iniatilisation et configuration ***
+  // récupérer le nombre de cases à cacher en fonction du niveau
+  var nb_case_vide = document.getElementById("niveau").value;
+  // nombre maximal d'essais à faire
+  var nb_max_loop = 1000;
+
+  //Décalaration de variables
+  var grille = new Array();
+  var lignes = new Array();
+  var colonnes = new Array();
+  var carres = new Array();
+  var i_while = 0;
+  var grille_complete = false;
+
+  // Vider les cases à chaque boucle
+  outerwhile: // Point de référence
+  while ((i_while < nb_max_loop) && !grille_complete) // Boucle tant que la grille n'est pas complète et que l'on n'a pas dépassé le maximum de boucle
+  {
+      i_while++; // On ajoute 1 à la boucle
+      // ...
+      // *** Initialisation des tableaux ***
+      for (var i = 1; i <= 9; i++)
+      {
+          grille[i] = new Array(); // On crée chaque ligne de la grille
+          lignes[i] = new Array(); // On crée un array pour les lignes
+          colonnes[i] = new Array(); // On crée un array pour les colonnes
+
+          for (var j = 1; j <= 9; j++)
+          {
+              grille[i][j] = 0; // On passe toutes les cases à 0
+              lignes[i][j] = j; // On complète toutes les possibilités de la ligne
+              colonnes[i][j] = j; // On complète toutes les possibilités de la colonne
+          };
+      };
+
+      for (var i = 1; i <= 3; i++)
+      {
+          carres[i] = new Array(); // On crée les trois lignes de carrés
+
+          for (var j = 1; j <= 3; j++)
+          {
+              carres[i][j] = new Array(); // On crée les trois colonnes de carrés dans chaque ligne de carré
+              for (var k = 1; k <= 9; k++)
+              {
+                  carres[i][j][k] = k; // Et on associe à chaque carré toutes les possibilités de lignes et de colonnes
+              };
+          };
+      };
+
+      // *** Calculs ***
+      // calculs cases par cases. Faire une boucle pour les lignes (y), suivie d'une boucle pour les colonnes (x) :
+      for (var y = 1; y <= 9; y++)
+      {
+          for (var x = 1; x <= 9; x++)
+          {
+            var possibilites = new Array();
+            var index = 0;
+
+            for (var k = 1; k <= 9; k++)
+            {
+                if (!lignes[y][k]) continue;
+                if (!colonnes[x][k]) continue;
+                if (!carres[Math.ceil(y/3)][Math.ceil(x/3)][k]) continue;
+
+                possibilites[index] = k;
+                index++;
+            };
+          };
+      };
+  };
+
+
+
 };
 
 function AjoutOptionAuSelect(this_select)
